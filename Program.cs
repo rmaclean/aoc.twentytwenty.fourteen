14af253cd8a1ee5f21ddd26b58122705ceafced6
Robert MacLean <robert@sadev.co.za>
2020-12-15 00:09:18 +0200

    part 2

--- Program.cs
+++ Program.cs
@@ -7,7 +7,8 @@ using System.IO;
 var data = (await File.ReadAllLinesAsync("data.txt")).ToList();
 char[] mask = new char[0];
 var instructions = data
-    .Select(line => {
+    .Select(line =>
+    {
         if (line.StartsWith("mask"))
         {
             return new Instruction(InstructionType.Mask, BuildMask(line), null);
@@ -16,7 +17,7 @@ var instructions = data
         return new Instruction(InstructionType.Memory, null, new MemoryInstruction(line));
     });
 
-var memory = new Dictionary<int, long>();
+var memory = new Dictionary<long, long>();
 
 foreach (var instruction in instructions)
 {
@@ -26,35 +27,65 @@ foreach (var instruction in instructions)
     }
     else
     {
-        memory[instruction.memoryInstruction.Location] = Convert.ToInt64(ApplyMask(instruction.memoryInstruction.InitialValue), 2);
+        var addressesToSet = ApplyMask(instruction.memoryInstruction.Location);
+        foreach (var addressToSet in addressesToSet)
+        {
+            memory[addressToSet] = instruction.memoryInstruction.InitialValue;
+        }
     }
 }
+
+// foreach (var item in memory)
+// {
+//     Console.WriteLine($"Memory {item.Key}");
+//     Console.WriteLine($"\t{item.Value.ToBits()} (decimal {item.Value})");
+// }
 
 var answer = memory.Aggregate(0L, (curr, next) => curr += next.Value);
 Console.WriteLine($"Answer is {answer}");
 
 char[] BuildMask(string line) => line.Split("=")[1].Trim().ToArray();
 
-string ApplyMask(long value)
+List<long> ApplyMask(long value)
 {
     var bitValue = value.ToBits().ToArray();
-    var result = new char[bitValue.Length];
+    var results = new List<char[]> { new char[bitValue.Length] };
+
     for (var index = 0; index < bitValue.Length; index++)
     {
         var bit = bitValue[index];
         var maskBit = mask[index];
 
-        if (maskBit == 'X')
+        switch (maskBit)
         {
-            result[index] = bit;
-        } 
-        else
-        {
-            result[index] = maskBit;
+            case '0':
+                {
+                    foreach(var result in results) result[index] = bit;
+                    break;
+                }
+            case '1':
+                {
+                    foreach(var result in results) result[index] = '1';
+                    break;
+                }
+            case 'X':
+                {
+                    var newVersions = new List<char[]>();
+                    foreach(var result in results)
+                    {
+                        var tmp = (char[])result.Clone();
+                        result[index] = '1';
+                        tmp[index] = '0';
+                        newVersions.Add(tmp);
+                    }
+
+                    results.AddRange(newVersions);
+                    break;
+                }
         }
     }
 
-    return new string(result);
+    return results.Select(c => new String(c)).Select(r => Convert.ToInt64(r, 2)).ToList();
 }
 
 public class MemoryInstruction
@@ -63,7 +94,7 @@ public class MemoryInstruction
     public long InitialValue { get; init; }
 
     private static Regex parser = new Regex("mem\\[(?<address>\\d+)] = (?<value>\\d+)");
-    
+
     public MemoryInstruction(string input)
     {
         var matches = parser.Matches(input);
